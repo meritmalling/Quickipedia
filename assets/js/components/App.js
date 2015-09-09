150fca9ada898a91c9def7512441d76546e8a3d7
cherylafitz <cherylafitzgerald@gmail.com>
2015-09-09 16:30:40 -0700

    added search bar in react

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,12 +1,41 @@
 var React = require('react');
+var SearchForm = require('./SearchForm');
 
-module.exports = React.createClass({
+var TuneSearch = React.createClass({
+  getInitialState: function(){
+    return {
+      searchTerm:'',
+      results: {}
+    };
+  },
+  updateSearch: function(value){
+    this.setState({searchTerm: value});
+    var self = this;
+    var ajax = new XMLHttpRequest();
+    ajax.addEventListener('load',function(){
+      try {
+        var data = JSON.parse(this.responseText);
+        console.log(data)
+        self.setState({results:data});
+      } catch(e) {
+        self.setState({results:{}});
+      }
+    });
+    ajax.open('GET','/summary?q=' + value);
+    ajax.send();
+  },
   render: function(){
+
     return (
-      <div>
-        <h1>Hello from react</h1>
+      <div className="row">
+        <div className="col-xs-6 col-xs-offset-3">
+          <SearchForm onUpdate={this.updateSearch} />
+          <div> {this.state.results.summary} </div>
+        </div>
       </div>
-    )
+    );
   }
 });
+
+module.exports = TuneSearch;
 

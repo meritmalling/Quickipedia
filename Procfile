03a5c2b2f256f09d5477d7f4e89d0bdb20d432b6
merittrompeter <mktrompeter@gmail.com>
2015-09-09 19:36:05 -0700

    testing heroku config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app --log-file=-
+web: python dealscraper/hello.py
--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def random():
         }
         return json.dumps(contents)
 
-app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
 
 
